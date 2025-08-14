53d410dcf25908564985809f0120e35e0d0091c5
abdulalimsujon <alimsujon12@gmail.com>
2025-08-14 16:39:44 +0600

    update

--- src/app/interface/index.ts
+++ src/app/interface/index.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-namespace */
-
-import { JwtPayload } from "jsonwebtoken";
+import { JwtPayload } from 'jsonwebtoken';
 
 declare global {
   namespace Express {
