5ebff7526cb5e91a76b16af2559ba24e552a5231
abdulalimsujon <alimsujon12@gmail.com>
2025-08-14 14:22:48 +0600

    update

--- src/app/interface/index.ts
+++ src/app/interface/index.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { JwtPayload } from 'jsonwebtoken';
+declare module 'cookie-parser';
+declare module 'jsonwebtoken';
+declare module 'bcrypt';
 
 declare global {
   namespace Express {
